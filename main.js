69e09b52d013ca31afebdd882a5602cd46f0df8c
Vini <vinicius.moooca.zl@gmail.com>
2022-08-04 19:11:03 -0300

    refatoração nome de variavel e função

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const poster = document.querySelector(".poster-filme");
 
 
 
-function fazGet(url) {
+function get(url) {
     let request = new XMLHttpRequest()
     request.open("GET", url, false)
     request.send()
@@ -12,7 +12,7 @@ function fazGet(url) {
 
 }
 
-function movieFront(title, desc, post) {
+function updateFront(title, desc, post) {
     titulo.innerHTML = title;
     descricao.innerHTML = desc;
     poster.setAttribute('src', `https://image.tmdb.org/t/p/w500/${post}`)
@@ -22,17 +22,13 @@ function movieFront(title, desc, post) {
 document.getElementById("btn-search").addEventListener("click", function main() {
 
     let id = (Math.floor(Math.random() * (957 - 0) + 0));
-    let data = fazGet(`https://api.themoviedb.org/3/movie/${id}?api_key=15c67dc9072a44e6666013d769223ca6`)
+    let data = get(`https://api.themoviedb.org/3/movie/${id}?api_key=15c67dc9072a44e6666013d769223ca6`)
     let filmes = JSON.parse(data);
     const title = filmes.original_title;
     const desc = filmes.overview;
     const posterPath = filmes.poster_path;
     console.log(id)
 
-    movieFront(title, desc, posterPath)
+    updateFront(title, desc, posterPath)
 
-}
-
-)
-
-
+})
