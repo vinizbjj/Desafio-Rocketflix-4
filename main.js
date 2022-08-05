1fcbdda920490af7ab4ef929e7bee8f2076c9201
vinizbjj <viniz.bjj@gmail.com>
2022-08-05 16:27:06 -0300

    ajuste do botão de clique

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const titulo = document.querySelector(".title-filme");
 const descricao = document.querySelector(".desc-filme");
 const poster = document.querySelector(".poster-filme");
+let adjustbtn = document.getElementById("btn-search")
 
 
 
@@ -20,7 +21,7 @@ function updateFront(title, desc, post) {
 
 
 document.getElementById("btn-search").addEventListener("click", function main() {
-
+    adjustbtn.style.marginTop = "25%";
     let id = (Math.floor(Math.random() * (957 - 0) + 0));
     let data = get(`https://api.themoviedb.org/3/movie/${id}?api_key=15c67dc9072a44e6666013d769223ca6`)
     let filmes = JSON.parse(data);
