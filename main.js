1d312506710ce52e11ae8e2ee4b7278ecac51aa2
vinizbjj <viniz.bjj@gmail.com>
2022-08-08 16:30:40 -0300

    Adicionado dias da semana para o codigo

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ import { API_KEY, BASE_URL } from "./api.js";
 const titulo = document.querySelector(".title-filme");
 const descricao = document.querySelector(".desc-filme");
 const poster = document.querySelector(".poster-filme");
+const postUndefined = document.querySelector(".poster-filme")
 let adjustBtn = document.getElementById("btn-search");
 let posterCard = document.getElementById("poster-Card")
+let titleFilme = document.getElementById("title")
 
 
 
@@ -22,22 +24,47 @@ function updateFront(title, desc, post) {
     poster.setAttribute('src', `https://image.tmdb.org/t/p/w500/${post}`)
 }
 
+function createFront(titleSegunda, descSegunda) {
+    titulo.innerHTML = titleSegunda;
+    descricao.innerHTML = descSegunda;
+    postUndefined.setAttribute('src', "./assets/study.png")
+}
+
 
 document.getElementById("btn-search").addEventListener("click", function main() {
-    
-    let id = (Math.floor(Math.random() * (999999 - 0) + 0));
-    let data = get(`${BASE_URL}${id}?api_key=${API_KEY}`)
-    console.log(data)
-    let filmes = JSON.parse(data);
-    const title = filmes.original_title;
-    const desc = filmes.overview;
-    const posterPath = filmes.poster_path;
 
-    adjustBtn.style.marginTop = "25%";
-    posterCard.style.display = "flex";
-    
-    console.log(id)
+    let semana = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado"];
+    let d = new Date();
 
-    updateFront(title, desc, posterPath)
+    if (semana[d.getDay()] == "Segunda-Feira") {
+        const titleSegunda = ""
+        const descSegunda = "Ops, hoje não é dia de assistir filme. Bora codar! 🚀";
+        const postSegunda = ('src', "./assets/study.png")
+
+        titleFilme.style.marginTop = "22%";
+        adjustBtn.style.marginTop = "25%";
+        posterCard.style.display = "flex";
+        createFront(titleSegunda, descSegunda, postSegunda)
+
+    } else {
+
+        let id = (Math.floor(Math.random() * (999999 - 0) + 0));
+        let data = get(`${BASE_URL}${id}?api_key=${API_KEY}`)
+        console.log(data)
+        let filmes = JSON.parse(data);
+        const title = filmes.original_title;
+        const desc = filmes.overview;
+        const posterPath = filmes.poster_path;
+
+        adjustBtn.style.marginTop = "25%";
+        posterCard.style.display = "flex";
+
+        console.log(id)
+        updateFront(title, desc, posterPath)
+    }
+
+
 
 })
+
+
